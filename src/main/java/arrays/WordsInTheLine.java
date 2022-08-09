2299f1871ad681f4cdf008d57047634eff393666
Darya Bushik <darya.bushik@gmail.com>
2022-08-09 13:56:22 +0300

    add task

---
+++ src/main/java/arrays/WordsInTheLine.java
@@ -0,0 +1,14 @@
+/**
+ * Напишите программу, которая определяет, сколько слов в сроке (ввод с консоли).
+ */
+package arrays;
+
+public class WordsInTheLine {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1; i++) {
+            System.out.println(args.length);
+        }
+    }
+}
