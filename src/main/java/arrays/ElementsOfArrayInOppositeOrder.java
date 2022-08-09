36fa16e42c0f88c60ab3cea3b3c85f3e77f51539
Darya Bushik <darya.bushik@gmail.com>
2022-08-09 13:50:52 +0300

    add task

---
+++ src/main/java/arrays/ElementsOfArrayInOppositeOrder.java
@@ -0,0 +1,16 @@
+/**
+ * Напишите программу, которая меняет местами элементы одномерного массива из String в обратном порядке.
+ * Не используйте дополнительный массив для хранения результатов.
+ */
+package arrays;
+
+public class ElementsOfArrayInOppositeOrder {
+
+    public static void main(String[] args) {
+
+        String[] letters = {"A", "B", "C", "D"};
+        for (int i = letters.length - 1; i >= 0; i--) {
+            System.out.print(letters[i] + " ");
+        }
+    }
+}
