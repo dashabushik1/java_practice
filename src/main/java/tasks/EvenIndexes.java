364e2387f901711fd60dfad29867d2d798d3de36
Darya Bushik <darya.bushik@gmail.com>
2022-08-09 20:51:12 +0300

    add task

---
+++ src/main/java/tasks/EvenIndexes.java
@@ -0,0 +1,30 @@
+/**
+ * Создать второй массив из четных элементов первого массива.
+ */
+package tasks;
+
+public class EvenIndexes {
+
+    public static void main(String[] args) {
+
+        int[] firstArray = {1, 2, 3, 4, 5, 6};
+        int count = 0;
+        for (int i = 0; i < firstArray.length; i++) {
+            if (firstArray[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        int[] secondArray = new int[count];
+        int j = 0;
+        for (int i = 0; i < firstArray.length; i++) {
+            if (firstArray[i] % 2 == 0) {
+                secondArray[j] = firstArray[i];
+                j++;
+            }
+        }
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.print(secondArray[i] + " ");
+        }
+    }
+}
