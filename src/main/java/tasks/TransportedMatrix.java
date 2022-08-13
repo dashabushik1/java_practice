9b78abc6c0e8b5432f9034075cc49ffd170dca7a
Darya Bushik <darya.bushik@gmail.com>
2022-08-13 22:49:18 +0300

    changed task

---
+++ src/main/java/tasks/TransportedMatrix.java
@@ -0,0 +1,41 @@
+/**
+ * Напишите программу, которая будет транспортировать заданную квадратную матрицу без использования дополнительного массива.
+ * Транспортированная матрица - это матрица, полученная из исходной матрицы путем замены строк на столбцы.
+ * [1 2]   [1 3]
+ * [3 4]   [2 4]
+ */
+package tasks;
+
+public class TransportedMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] squareMatrix = {{1, 2}, {3, 4}};
+        System.out.println("Initial matrix: ");
+        for (int i = 0; i < squareMatrix.length; i++) {
+            for (int j = 0; j < squareMatrix[i].length; j++) {
+                System.out.printf("%4d", squareMatrix[i][j]);
+            }
+            System.out.println("");
+        }
+
+        for (int i = 0; i < squareMatrix.length; i++) {
+            for (int j = i + 1; j < squareMatrix[i].length; j++) {
+                if (i != j) {
+                    int t = squareMatrix[i][j];
+                    squareMatrix[i][j] = squareMatrix[j][i];
+                    squareMatrix[j][i] = t;
+                }
+            }
+        }
+
+        System.out.println("Transported matrix: ");
+        for (int i = 0; i < squareMatrix.length; i++) {
+            for (int j = 0; j < squareMatrix[i].length; j++) {
+                System.out.printf("%4d", squareMatrix[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
+
