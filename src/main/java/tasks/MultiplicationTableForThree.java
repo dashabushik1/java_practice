aab6b05b955d7a206dd8dd42f5eb6657ee17a06a
Darya Bushik <darya.bushik@gmail.com>
2022-08-19 22:45:22 +0300

    add method to count multiplication table

--- src/main/java/tasks/MultiplicationTableForThree.java
+++ src/main/java/tasks/MultiplicationTableForThree.java
@@ -7,10 +7,20 @@ public class MultiplicationTableForThree {
 
     public static void main(String[] args) {
 
-        for (int i = 3; i <= 3 ; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.println(i + " * " + j + " = " + (i*j));
-            }
+        final int number = 3;
+        countMultiplicationTable(number);
+
+//        for (int i = 3; i <= 3 ; i++) {
+//            for (int j = 1; j <= 10; j++) {
+//                System.out.println(i + " * " + j + " = " + (i*j));
+//            }
+//        }
+    }
+
+    public static void countMultiplicationTable(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " * " + n + " = " + (i * n));
         }
     }
+    // метод вывода?
 }
