69fc8c5b0d5729f0a9cb20c8f934476273aebcb0
Darya Bushik <darya.bushik@gmail.com>
2022-08-19 23:37:06 +0300

    change variable

--- src/main/java/tasks/NumbersFromSeven.java
+++ src/main/java/tasks/NumbersFromSeven.java
@@ -8,8 +8,8 @@ public class NumbersFromSeven {
 
     public static void main(String[] args) {
 
-        int count = 100;
-        for (int i = 7; i < count; i = i + 7) {   // 1 способ
+        int maxNumber = 100;
+        for (int i = 7; i < maxNumber; i = i + 7) {   // 1 способ
             System.out.print(i + " ");
         }
 
