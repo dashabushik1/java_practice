b25cc942753976d5cdc4fc94b71f6f4dcfda8a5a
Darya Bushik <darya.bushik@gmail.com>
2022-08-20 15:57:58 +0300

    add methods of fill and print array

--- src/main/java/tasks/StarsOutput.java
+++ src/main/java/tasks/StarsOutput.java
@@ -1,8 +1,8 @@
 /**
- * Нербходимо написать программу, которая вывела бы в консоль звездочки вот так образом, как на картинке ниже.
- * * * * * *
- * * * * * *
- * * * * * *
+ * Необходимо написать программу, которая вывела бы в консоль звездочки вот так образом, как на картинке ниже.
+ *   * * * * *
+ *   * * * * *
+ *   * * * * *
  */
 package tasks;
 
@@ -10,11 +10,31 @@ public class StarsOutput {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print("* ");
+        final int rows = 3;
+        final int columns = 5;
+        String[][] starsArray = new String[rows][columns];
+
+        fillStarsArray(starsArray);
+        printArrayOfStars(starsArray);
+    }
+
+    public static String[][] fillStarsArray(String[][] starsArray) {   // метод заполняет двумерный массив звездочками
+        String star = "*";
+        for (int i = 0; i < starsArray.length; i++) {
+            for (int j = 0; j < starsArray[i].length; j++) {
+                starsArray[i][j] = star;
+            }
+        }
+        return starsArray;
+    }
+
+    public static String[][] printArrayOfStars(String[][] starsArray) {  // метод выводит двумерный массив со звездочками
+        for (int i = 0; i < starsArray.length; i++) {
+            for (int j = 0; j < starsArray[i].length; j++) {
+                System.out.print(starsArray[i][j] + " ");
             }
             System.out.println("");
         }
+        return starsArray;
     }
 }
