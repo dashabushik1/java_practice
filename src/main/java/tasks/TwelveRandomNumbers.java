618a15c27a22feb4e023370736da05d71bf06757
Darya Bushik <darya.bushik@gmail.com>
2022-08-20 18:54:44 +0300

    add methods, change Math.random, change variable

--- src/main/java/tasks/TwelveRandomNumbers.java
+++ src/main/java/tasks/TwelveRandomNumbers.java
@@ -8,19 +8,37 @@ public class TwelveRandomNumbers {
 
     public static void main(String[] args) {
 
-        int[] randomNumbers = new int[12];
-        int maxIndex = 0;
+        final int SIZE = 12;
+        int[] randomNumbers = new int[SIZE];
+
+        fillArrayOfRandomNumbers(randomNumbers);
+        printArrayOfRandomNumbers(randomNumbers);
+        printIndexOfMaxElementInArray(randomNumbers);
+    }
+
+    public static int[] fillArrayOfRandomNumbers(int[] randomNumbers) { // метод заполняет массив случайными числами
         for (int i = 0; i < randomNumbers.length; i++) {
-            randomNumbers[i] = (int) ((Math.random() * 30) - 15);
+            randomNumbers[i] = (int) ((Math.random() * 31) - 15);
+        }
+        return randomNumbers;
+    }
+
+    public static int[] printArrayOfRandomNumbers(int[] randomNumbers) { // метод выводит рандомные числа
+        for (int i = 0; i < randomNumbers.length; i++) {
             System.out.print(randomNumbers[i] + " ");
         }
+        return randomNumbers;
+    }
 
-        System.out.println(" ");
+    public static int[] printIndexOfMaxElementInArray(int[] randomNumbers) { // метод ищет и выводит индекс
+        int maxIndex = 0;                // максимального элемента в массиве
         for (int i = 0; i < randomNumbers.length; i++) {
             if (randomNumbers[i] >= randomNumbers[maxIndex]) {
                 maxIndex = i;
             }
         }
+        System.out.println("");
         System.out.println(maxIndex);
+        return randomNumbers;
     }
 }
