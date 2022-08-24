4394f8093128a2c2c7355ac82dab4c64ae8cf97f
Darya Bushik <darya.bushik@gmail.com>
2022-08-24 19:10:52 +0300

    add method

--- src/main/java/tasks/WordsInTheLine.java
+++ src/main/java/tasks/WordsInTheLine.java
@@ -7,6 +7,17 @@ public class WordsInTheLine {
 
     public static void main(String[] args) {
 
-        System.out.println(args.length);
+        String sentence = "She served a big table in the dining room.";
+
+        countWords(sentence);
+    }
+
+    public static void countWords(String string) {
+        String[] arr = string.split(" ");
+        int count = 0;
+        for (String str : arr) {
+            count++;
+        }
+        System.out.println(count);
     }
 }
