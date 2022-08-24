f07c57942b9d3c028c1aabbb07576824cfc51a18
Darya Bushik <darya.bushik@gmail.com>
2022-08-24 13:00:41 +0300

    change variables, change names of methods and parameters

--- src/main/java/tasks/StarsOutput.java
+++ src/main/java/tasks/StarsOutput.java
@@ -1,40 +1,39 @@
 /**
  * Необходимо написать программу, которая вывела бы в консоль звездочки вот так образом, как на картинке ниже.
- *   * * * * *
- *   * * * * *
- *   * * * * *
+ * * * * * *
+ * * * * * *
+ * * * * * *
  */
 package tasks;
 
 public class StarsOutput {
 
+    private static final int ROWS = 3;
+    private static final int COLUMNS = 5;
+
     public static void main(String[] args) {
 
-        final int rows = 3;
-        final int columns = 5;
-        String[][] starsArray = new String[rows][columns];
+        String[][] starsArray = new String[ROWS][COLUMNS];
 
-        fillStarsArray(starsArray);
-        printArrayOfStars(starsArray);
+        fill(starsArray);
+        print(starsArray);
     }
 
-    public static String[][] fillStarsArray(String[][] starsArray) {   // метод заполняет двумерный массив звездочками
+    public static void fill(String[][] array) {   // метод заполняет двумерный массив звездочками
         String star = "*";
-        for (int i = 0; i < starsArray.length; i++) {
-            for (int j = 0; j < starsArray[i].length; j++) {
-                starsArray[i][j] = star;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = star;
             }
         }
-        return starsArray;
     }
 
-    public static String[][] printArrayOfStars(String[][] starsArray) {  // метод выводит двумерный массив со звездочками
-        for (int i = 0; i < starsArray.length; i++) {
-            for (int j = 0; j < starsArray[i].length; j++) {
-                System.out.print(starsArray[i][j] + " ");
+    public static void print(String[][] array) {  // метод выводит двумерный массив со звездочками
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
             }
             System.out.println("");
         }
-        return starsArray;
     }
 }
