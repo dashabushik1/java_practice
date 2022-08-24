fbfd3a362b679a1d028c3294a544025975f818a5
Darya Bushik <darya.bushik@gmail.com>
2022-08-24 16:56:53 +0300

    add methods, change variables

--- src/main/java/tasks/NumbersFromOne.java
+++ src/main/java/tasks/NumbersFromOne.java
@@ -6,12 +6,42 @@ package tasks;
 
 public class NumbersFromOne {
 
+//    public static void main(String[] args) {   // метод с циклом do/while
+//
+//        int multiply = 1;
+//        int i = 1;
+//        int firstNumber = 1;
+//        System.out.print(firstNumber + " ");
+//        do {
+//            multiply = multiply * 2;
+//            i++;
+//            System.out.print(multiply + " ");
+//        } while (i < 10);
+//    }
+
+    private static final int MAX_NUMBER = 512;
+    private static final int LENGTH_OF_ARRAY = 10;
+
     public static void main(String[] args) {
 
-        int multiply = 1;
-        for (int i = 1; i < 10; i++) {
-            multiply = multiply * 2;
-            System.out.print(multiply + " ");  // не выводится 1
+        int[] sequenceOfNumbers = new int[LENGTH_OF_ARRAY];
+
+        fillArrayBySequenceOfNumbers(sequenceOfNumbers, MAX_NUMBER);
+        print(sequenceOfNumbers);
+    }
+
+    public static void fillArrayBySequenceOfNumbers(int[] array, int max) {  // метод принимает максимальное число
+        for (int i = array.length - 1; i >= 0; i--) {   // и заполняет массив последовательностью чисел до этого числа
+            array[i] = max;
+            max = max / 2;
+        }
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                System.out.print(array[i] + " ");
+            }
         }
     }
 }
