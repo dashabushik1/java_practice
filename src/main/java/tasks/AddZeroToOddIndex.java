0346af954c4a382dec9c07679327c45ef6f8da0e
Darya Bushik <darya.bushik@gmail.com>
2022-08-24 11:46:32 +0300

    change variable, names of methods, names of arguments and return types

--- src/main/java/tasks/AddZeroToOddIndex.java
+++ src/main/java/tasks/AddZeroToOddIndex.java
@@ -6,39 +6,37 @@ package tasks;
 
 public class AddZeroToOddIndex {
 
+    private static final int SIZE = 8;
+
     public static void main(String[] args) {
 
-        final int SIZE = 8;
         int[] randomNumbers = new int[SIZE];
 
-        fillArrayOfRandomNumbers(randomNumbers);
-        printArray(randomNumbers);
+        fillArrayByRandomNumbers(randomNumbers);
+        print(randomNumbers);
         System.out.println("");
         replaceElementsWithOddIndexes(randomNumbers);
-        printArray(randomNumbers);
+        print(randomNumbers);
     }
 
-    public static int[] fillArrayOfRandomNumbers(int[] randomNumbers) {  // метод заполняет массив случайными числами
-        for (int i = 0; i < randomNumbers.length; i++) {
-            randomNumbers[i] = (int) (Math.random() * 10) + 1;
+    public static void fillArrayByRandomNumbers(int[] array) {  // метод заполняет массив случайными числами
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10) + 1;
         }
-        return randomNumbers;
     }
 
-    public static int[] replaceElementsWithOddIndexes(int[] randomNumbers) {  // метод находит элементы с нечетным индексом
-        for (int i = 0; i < randomNumbers.length; i++) {                      // и меняет значения на 0
+    public static void replaceElementsWithOddIndexes(int[] array) {  // метод находит элементы с нечетным индексом
+        for (int i = 0; i < array.length; i++) {                      // и меняет значения на 0
             if (i % 2 != 0) {
-                randomNumbers[i] = 0;
+                array[i] = 0;
             }
         }
-        return randomNumbers;
     }
 
-    public static int[] printArray(int[] randomNumbers) {  // метод выводит массив
-        for (int i = 0; i < randomNumbers.length; i++) {
-            System.out.print(randomNumbers[i] + " ");
+    public static void print(int[] array) {  // метод выводит массив
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
-        return randomNumbers;
     }
 }
 
